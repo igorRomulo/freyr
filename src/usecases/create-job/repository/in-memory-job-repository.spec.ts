b40af4cd6e171d47c08428a8b8803357bfdea3b7
igor <igor.romulo.santos@gmail.com>
2022-06-21 08:56:45 -0300

    test: add, find and check existence jobs

--- src/usecases/create-job/repository/in-memory-job-repository.spec.ts
+++ src/usecases/create-job/repository/in-memory-job-repository.spec.ts
@@ -3,9 +3,28 @@ import { InMemoryJobRepository } from './in-memory-job-repository'
 
 describe('In memory Job Repository', () => {
   test('should return null if user is not found', async () => {
+    // given
     const jobs: JobData[] = []
     const jobRepo = new InMemoryJobRepository(jobs)
+
+    // when
     const job = await jobRepo.findJobByName('any_job_name')
+
+    // then
     expect(job).toBeNull()
+  })
+
+  test('should return job if it is found in the repository', async () => {
+    // given
+    const jobs: JobData[] = []
+    const name = 'any_job_name'
+    const jobRepo = new InMemoryJobRepository(jobs)
+
+    // when
+    await jobRepo.add({ name })
+    const job = await jobRepo.findJobByName(name)
+
+    // then
+    expect(job.name).toBe(name)
   })
 })
