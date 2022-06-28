c3ec1a4fdec4f320d21e67ed66a18df08c73ae33
igor <igor.romulo.santos@gmail.com>
2022-06-28 08:17:51 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -4,6 +4,13 @@ export class Email {
       return false
     }
 
+    const [local] = email.split('@')
+    const localPartTotalOfChars = 64
+
+    if (local.length > localPartTotalOfChars) {
+      return false
+    }
+
     return true
   }
 }
