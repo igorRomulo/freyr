613afb5ced3f74baf567c39570a44a7b0eb0f66c
igor <igor.romulo.santos@gmail.com>
2022-06-28 08:50:06 -0300

    test: validate emails with empty local part

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -30,4 +30,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@gmail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty local part', () => {
+    const email = '@gmail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
