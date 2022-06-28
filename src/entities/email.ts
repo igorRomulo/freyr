1c94f881264a45bca40b56d4aa1a7549f03c0004
igor <igor.romulo.santos@gmail.com>
2022-06-28 08:42:27 -0300

    feat: validate emails with domain larger than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -4,11 +4,16 @@ export class Email {
       return false
     }
 
-    const [local] = email.split('@')
+    const [local, domain] = email.split('@')
     const localPartTotalOfChars = 64
+    const domainTotalOfChars = 255
     const totalCharsEmail = 320
 
     if (email.length > totalCharsEmail) {
+      return false
+    }
+
+    if (domain.length > domainTotalOfChars) {
       return false
     }
 
